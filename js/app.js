ac822418f8d520c3b1fae4e5f02538fe9b20ba47
acox1993 <alexandriacrmu@gmail.com>
2016-06-15 08:59:19 -0400

    work w/ eric

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 $(document).ready(function(){
 	box = 0;
+	function goWin(value) {
+		$('div.results').show().html("<p>The winner is " + value + "</p>")
+	};
+	$('div.starter').click(function(){
+
+		if ($(this).children('i').hasClass('exe')) {
+			$('div.starter').html("<i class='fa fa-circle-o oho' aria-hidden='true'></i>");
+			box = 1;
+		} 
+		else {
+			$('div.starter').html("<i class='fa fa-times exe' aria-hidden='true'></i>");
+			box = 0;
+		};
+	});
 	$('td').click(function(){
-		box++;
-		if (box%2 == 0) {
-			$(this).html("").append("<i class='fa fa-circle-o oho' aria-hidden='true'></i>");
+		$('div.starter').hide();
+		if ($(this).children('i').length > 0) {
+			console.log('picked');
 		}
 		else {
-			$(this).html("").append("<i class='fa fa-times exe' aria-hidden='true'></i>");
+			box++;
+			if (box%2 == 0) {
+				$(this).html("").append("<i class='fa fa-circle-o oho' aria-hidden='true'></i>");
+			}
+			else {
+				$(this).html("").append("<i class='fa fa-times exe' aria-hidden='true'></i>");
+			};
 		};
-			//should i do with if's or cases?
+
+		$.each(["oho", "exe"], function(index, value) {
+				if ($('.one').children('i').hasClass(value) &&  $('.two').children('i').hasClass(value) && $('.three').children('i').hasClass(value)) {
+					goWin(value);	
+				};
+		});
+
+	});
+		
 		//if .one, .two, .three 
 		//if .four, .five, .six
 		//if .seven, .eight, .nine
@@ -17,5 +45,11 @@ $(document).ready(function(){
 		//if .three, .six, .nine
 		//if .one, .five, .nine
 		//if .three, .five, .seven
-	});
+	
+		//make results div
+		//highlighted to show which ones made victory
+
 });
+
+
+// restart button and functionality
