cd348ecacc01670e0176da53a6d4286c8750b706
acox1993 <alexandriacrmu@gmail.com>
2016-06-15 11:19:38 -0400

    readable

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	box = 0;
+
+	//win sequence//
 	function goWin(value) {
 		$('div.results').show().html("<p>The winner is <i class='fa " + value + "'aria-hidden='true'></i></p>")
 		if (value == 'fa-times') {
@@ -14,7 +16,7 @@ $(document).ready(function(){
 	};
 
 	$('div.starter').click(function(){
-
+		//choosing who will go first//
 		if ($(this).children('i').hasClass('exe')) {
 			$('div.starter').html("<i class='fa fa-circle-o oho' aria-hidden='true'></i>");
 			box = 1;
@@ -24,6 +26,7 @@ $(document).ready(function(){
 			box = 0;
 		};
 	});
+	//clicking a square on board//
 	$('td').click(function(){
 		$('div.starter').css('visibility', 'hidden');
 		if ($(this).children('i').length > 0) {
@@ -38,8 +41,9 @@ $(document).ready(function(){
 				$(this).html("").append("<i class='fa fa-times exe' aria-hidden='true'></i>");
 			};
 		};
-
+			//ability to win//
 		$.each(["fa-circle-o", "fa-times"], function(index, value) {
+				//horizontal//
 				if ($('.one').children('i').hasClass(value) &&  $('.two').children('i').hasClass(value) && $('.three').children('i').hasClass(value)) {
 					goWin(value);	
 				}
@@ -49,6 +53,7 @@ $(document).ready(function(){
 				else if ($('.seven').children('i').hasClass(value) &&  $('.eight').children('i').hasClass(value) && $('.nine').children('i').hasClass(value)) {
 					goWin(value);	
 				}
+				//vertical//
 				else if ($('.one').children('i').hasClass(value) &&  $('.four').children('i').hasClass(value) && $('.seven').children('i').hasClass(value)) {
 					goWin(value);	
 				}
@@ -58,6 +63,7 @@ $(document).ready(function(){
 				else if ($('.three').children('i').hasClass(value) &&  $('.six').children('i').hasClass(value) && $('.nine').children('i').hasClass(value)) {
 					goWin(value);	
 				}
+				//diagonal//
 				else if ($('.one').children('i').hasClass(value) &&  $('.five').children('i').hasClass(value) && $('.nine').children('i').hasClass(value)) {
 					goWin(value);	
 				}
@@ -67,17 +73,17 @@ $(document).ready(function(){
 		});
 
 	});
-	
+	//New Game//
 	function goNew() {
 		box = 0;
 		$('div.starter').css('visibility', 'visible');
 		$('div.results').hide();
 		$('td').html("");
-	};
+	}; 
 	$('.new').click(function(){
 		goNew();
 	});	
-
+    //Refreshes tally chart and game//
 	$('.restart').click(function(){
 		goNew();
 		$('.tallyWrapper p').html("");
